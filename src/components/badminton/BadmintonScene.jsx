543c8f1754d27c4a6b5cc3a4fe8d00168d12afc3
aryamantepal <aryamandora@gmail.com>
2023-11-11 17:27:15 -0500

    values changed

--- src/components/badminton/BadmintonScene.jsx
+++ src/components/badminton/BadmintonScene.jsx
@@ -2,31 +2,42 @@ import React, { useState } from 'react'
 import 'react-calendar/dist/Calendar.css';
 import './index.css'
 import bad from '../badminton/badminton.jpg'
-import Calendly from './calendly.jsx';
+// import Calendly from './calendly.jsx';
 import SchedulingComponent from './cals/cal';
 
 const BadmintonScene = () => {
     const [showCalendar1, setShowCalendar1] = useState(false);
     const [showCalendar2, setShowCalendar2] = useState(false);
     const [showCalendar3, setShowCalendar3] = useState(false);
+
+    const [bookedTimes1, setBookedTimes1] = useState([]);
+    const [bookedTimes2, setBookedTimes2] = useState([]);
+    const [bookedTimes3, setBookedTimes3] = useState([]);
+
     const handleClick = (calendarNumber) => {
+        setShowCalendar1(false);
+        setShowCalendar2(false);
+        setShowCalendar3(false);
+
         if (calendarNumber === 1) {
-            setShowCalendar1(!showCalendar1);
-            setShowCalendar2(false);
-            setShowCalendar3(false);
-            console.log("1")
+            setShowCalendar1(true);
         } else if (calendarNumber === 2) {
-            setShowCalendar1(false);
-            setShowCalendar2(!showCalendar2);
-            setShowCalendar3(false);
-            console.log("2")
+            setShowCalendar2(true);
         } else if (calendarNumber === 3) {
-            setShowCalendar1(false);
-            setShowCalendar2(false);
-            setShowCalendar3(!showCalendar3);
-            console.log("3")
+            setShowCalendar3(true);
         }
     };
+    const handleBooking = (courtNumber, bookedTime) => {
+        // Update the booked times for the specific court
+        if (courtNumber === 1) {
+            setBookedTimes1([...bookedTimes1, bookedTime]);
+        } else if (courtNumber === 2) {
+            setBookedTimes2([...bookedTimes2, bookedTime]);
+        } else if (courtNumber === 3) {
+            setBookedTimes3([...bookedTimes3, bookedTime]);
+        }
+    };
+
     return (
         <div>
             <h1>Badminton Courts</h1>
@@ -68,9 +79,9 @@ const BadmintonScene = () => {
                         <Calendar />
                     </div>
                 )} */}
-                {showCalendar1 && <SchedulingComponent />}
-                {showCalendar2 && <SchedulingComponent />}
-                {showCalendar3 && <SchedulingComponent />}
+                {showCalendar1 && <SchedulingComponent courtNumber={1} onBooking={handleBooking} bookedTimes={bookedTimes1} />}
+                {showCalendar2 && <SchedulingComponent courtNumber={2} onBooking={handleBooking} bookedTimes={bookedTimes2} />}
+                {showCalendar3 && <SchedulingComponent courtNumber={3} onBooking={handleBooking} bookedTimes={bookedTimes3} />}
             </div>
         </div>
     )
--- src/components/badminton/cals/cal.jsx
+++ src/components/badminton/cals/cal.jsx
@@ -1,91 +1,7 @@
-// import React, { useState } from 'react';
-
-// const SchedulingComponent = () => {
-//     const [selectedDate, setSelectedDate] = useState(new Date());
-//     const [selectedTime, setSelectedTime] = useState('');
-
-//     const handleDateChange = (date) => {
-//         setSelectedDate(date);
-//         // You may want to implement logic for fetching available time slots for the selected date
-//     };
-
-//     const handleTimeSelection = (time) => {
-//         setSelectedTime(time);
-//         // You can add further logic here based on the selected time
-//     };
-
-//     const handleScheduleAppointment = () => {
-//         // Implement logic for scheduling the appointment, e.g., send data to a server
-//         console.log('Appointment scheduled:', {
-//             date: selectedDate.toDateString(),
-//             time: selectedTime,
-//         });
-
-//         // Reset state after scheduling
-//         setSelectedDate(new Date());
-//         setSelectedTime('');
-//     };
-
-//     const handlePostRequest = () => {
-//         fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users')
-//             .then(response => response.json())
-//             .then(data => console.log(data));
-
-//     }
-//     return (
-//         <div>
-//             <h1>Schedule an Appointment</h1>
-
-//             <div>
-//                 <label>Select Date:</label>
-//                 <input
-//                     type="date"
-//                     value={selectedDate.toISOString().split('T')[0]}
-//                     onChange={(e) => handleDateChange(new Date(e.target.value))}
-//                 />
-//             </div>
-
-//             {/* Display available time slots (you may fetch this dynamically) */}
-//             <div>
-//                 <label>Select Time:</label>
-//                 <select value={selectedTime} onChange={(e) => handleTimeSelection(e.target.value)}>
-//                     <option value="">Select Time</option>
-// <option value="08:00 AM">08:00 AM</option>
-// <option value="09:00 AM">09:00 AM</option>
-// <option value="10:00 AM">10:00 AM</option>
-// <option value="11:00 AM">11:00 AM</option>
-// <option value="12:00 PM">12:00 PM</option>
-// <option value="01:00 PM">01:00 PM</option>
-// <option value="02:00 PM">02:00 PM</option>
-// <option value="03:00 PM">03:00 PM</option>
-// <option value="04:00 PM">04:00 PM</option>
-// <option value="05:00 PM">05:00 PM</option>
-// <option value="06:00 PM">06:00 PM</option>
-// <option value="07:00 PM">07:00 PM</option>
-// <option value="08:00 PM">08:00 PM</option>
-// <option value="09:00 PM">09:00 PM</option>
-// <option value="10:00 PM">10:00 PM</option>
-//                 </select>
-//             </div>
-
-//             <button onClick={handleScheduleAppointment} disabled={!selectedDate || !selectedTime}>
-//                 Schedule Appointment
-//             </button>
-//             <button onClick={handlePostRequest}>
-//                 Fetch
-//             </button>
-//         </div>
-//     );
-// };
-
-// export default SchedulingComponent;
-
-
 import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-const SchedulingComponent = () => {
+const SchedulingComponent = ({ courtNumber, bookedTimes, onBooking }) => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedTime, setSelectedTime] = useState('');
     const { user } = useAuth0();
@@ -108,17 +24,20 @@ const SchedulingComponent = () => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-
-                    "booked": false,
-                    "email": user.email,
-                    "name": user.name
-
+                    booked: false,
+                    email: user.email,
+                    name: user.name,
+                    date: selectedDate.toISOString().split('T')[0],
+                    time: selectedTime,
                 }),
             });
 
             if (response.ok) {
                 // Request was successful
                 console.log('Appointment scheduled successfully');
+                console.log(selectedTime)
+                // Notify parent component about the booking
+                onBooking(courtNumber, `${selectedDate.toISOString().split('T')[0]} ${selectedTime}`);
             } else {
                 // Handle errors
                 console.error('Error scheduling appointment');
@@ -132,19 +51,9 @@ const SchedulingComponent = () => {
         setSelectedTime('');
     };
 
-    const handlePostRequest = async () => {
-        try {
-            const response = await fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users');
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
-
     return (
         <div>
-            <h1>Schedule an Appointment</h1>
+            <h1>Schedule an Appointment for Court Number {courtNumber}</h1>
             <div>
                 <label>Select Date:</label>
                 <input
@@ -160,28 +69,27 @@ const SchedulingComponent = () => {
                 <select value={selectedTime} onChange={(e) => handleTimeSelection(e.target.value)}>
                     <option value="">Select Time</option>
                     {/* ... (time options) */}
-                    <option value="08:00 AM">08:00 AM</option>
-                    <option value="09:00 AM">09:00 AM</option>
-                    <option value="10:00 AM">10:00 AM</option>
-                    <option value="11:00 AM">11:00 AM</option>
-                    <option value="12:00 PM">12:00 PM</option>
-                    <option value="01:00 PM">01:00 PM</option>
-                    <option value="02:00 PM">02:00 PM</option>
-                    <option value="03:00 PM">03:00 PM</option>
-                    <option value="04:00 PM">04:00 PM</option>
-                    <option value="05:00 PM">05:00 PM</option>
-                    <option value="06:00 PM">06:00 PM</option>
-                    <option value="07:00 PM">07:00 PM</option>
-                    <option value="08:00 PM">08:00 PM</option>
-                    <option value="09:00 PM">09:00 PM</option>
-                    <option value="10:00 PM">10:00 PM</option>
+                    <option value="8">08:00 AM</option>
+                    <option value="9">09:00 AM</option>
+                    <option value="10">10:00 AM</option>
+                    <option value="11">11:00 AM</option>
+                    <option value="12">12:00 PM</option>
+                    <option value="13">01:00 PM</option>
+                    <option value="14">02:00 PM</option>
+                    <option value="15">03:00 PM</option>
+                    <option value="16">04:00 PM</option>
+                    <option value="17">05:00 PM</option>
+                    <option value="18">06:00 PM</option>
+                    <option value="19">07:00 PM</option>
+                    <option value="20">08:00 PM</option>
+                    <option value="21">09:00 PM</option>
+                    <option value="22">10:00 PM</option>
                 </select>
             </div>
 
-            <button onClick={handleScheduleAppointment} disabled={!selectedDate || !selectedTime}>
+            <button onClick={handleScheduleAppointment} disabled={!selectedDate || !selectedTime || bookedTimes.includes(`${selectedDate.toISOString().split('T')[0]} ${selectedTime}`)}>
                 Schedule Appointment
             </button>
-            <button onClick={handlePostRequest}>Fetch</button>
         </div>
     );
 };
